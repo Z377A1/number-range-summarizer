724cf100dbb460ad41e018474e5ffb69629a46bd
Mlungisi Khwange <70942307+Z377A1@users.noreply.github.com>
2025-09-19 20:37:56 +0200

    Fix path to HTML report in test script

--- test-generate-reports.sh
+++ test-generate-reports.sh
@@ -6,4 +6,4 @@ echo "Generating reports for tests..."
 ./mvnw surefire-report:report-only
 
 echo "Opening html report in browser..."
-google-chrome --no-sandbox /workspaces/number-range-summarizer/target/reports/surefire.html &
+google-chrome --no-sandbox ./target/reports/surefire.html &
